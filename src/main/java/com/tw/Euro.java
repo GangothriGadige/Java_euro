5821e2f43210b3b87a7748b2ee4668e01f2f66ff
Himanshi <himanshi2002goel@gmail.com>
2022-07-05 19:05:17 +0530

    [Gangothri/Himanshi] Requirement #1 - Check wheter the euros are equal or not

--- src/main/java/com/tw/Euro.java
+++ src/main/java/com/tw/Euro.java
@@ -1,4 +1,16 @@
 package com.tw;
 
+import java.util.Objects;
+
 public class Euro {
+    private final int value;
+
+    Euro(int value) {
+        this.value = value;
+    }
+
+    public boolean checkEquality(Euro euro) {
+        return this.value == euro.value;
+    }
+
 }
--- src/test/java/com/tw/EuroTest.java
+++ src/test/java/com/tw/EuroTest.java
@@ -1,4 +1,31 @@
 package com.tw;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+
 public class EuroTest {
+    @Test
+    public void shouldCheckEqualityWhenTenEuroIsComparedWithTenEuro() {
+        Euro firstEuro = new Euro(10);
+        Euro secondEuro = new Euro(10);
+        boolean expectedValue = true;
+
+        boolean actualValue = firstEuro.checkEquality(secondEuro);
+
+        assertThat(actualValue, equalTo(expectedValue));
+    }
+
+    @Test
+    public void shouldCheckEqualityWhenTenEuroIsComparedWithTwentyEuro() {
+        Euro firstEuro = new Euro(10);
+        Euro secondEuro = new Euro(10);
+        boolean expectedValue = true;
+
+        boolean actualValue = firstEuro.checkEquality(secondEuro);
+
+        assertThat(actualValue, equalTo(expectedValue));
+    }
 }
